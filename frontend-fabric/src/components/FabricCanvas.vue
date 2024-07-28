e0c67d21e0bb7676eb74c340bd3e471cbc03af6e
sokumon <sohamkulkarns9@gmail.com>
2024-07-28 14:53:14 +0530

    fabric setup in vue works

---
+++ frontend-fabric/src/components/FabricCanvas.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex justify-center items-center h-screen bg-gray-100">
+    <canvas
+      ref="canvas"
+      id="hello"
+      class="border border-gray-400"
+      width="800"
+      height="600"
+    ></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FabCanvas',
+  mounted() {
+    this.canvas = new this.fabric.Canvas('hello')
+
+    // Add a rectangle
+    const rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: 'red',
+      width: 200,
+      height: 200,
+    })
+
+    this.canvas.add(rect)
+  },
+  data() {
+    return {
+      canvas: null,
+    }
+  },
+}
+</script>
---
+++ frontend-fabric/src/pages/Draw.vue
@@ -0,0 +1,24 @@
+<template>
+  <FabCanvas></FabCanvas>
+</template>
+
+<script>
+import FabCanvas from '@/components/FabricCanvas.vue'
+
+export default {
+  name: 'Draw',
+  data() {
+    return {
+      showDialog: false,
+    }
+  },
+  resources: {
+    ping: {
+      url: 'ping',
+    },
+  },
+  components: {
+    FabCanvas,
+  },
+}
+</script>
